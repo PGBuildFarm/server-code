fe7ec2278d5dfdb0cc4512a9822e7e315a66858d
Andrew Dunstan <andrew@dunslane.net>
2007-08-27 16:50:30 +0000

    get log text from build_status_log table

--- cgi-bin/show_stage_log.pl
+++ cgi-bin/show_stage_log.pl
@@ -27,6 +27,51 @@ my $stage = $query->param('stg');$stage =~ s/[^a-zA-Z0-9_ -]//g;
 
 use vars qw($tgz);
 
+if ($system && $logdate && $stage)
+{
+    my $db = DBI->connect($dsn,$dbuser,$dbpass);
+
+    die $DBI::errstr unless $db;
+
+    my $statement = q(
+
+        select branch, log_text
+        from build_status_log
+        where sysname = ? and snapshot = ? and log_stage = ? || '.log'
+
+        );
+
+
+    
+    my $sth=$db->prepare($statement);
+    $sth->execute($system,$logdate,$stage);
+    my $row=$sth->fetchrow_arrayref;
+    my ($branch, $logtext) = ("unknown","no log text found");
+    if ($row)
+    {
+        $branch = $row->[0];
+        $logtext =$row->[1];
+    }
+    $sth->finish;
+    $db->disconnect;
+
+    print "Content-Type: text/plain\n\n", $logtext,
+
+    "-------------------------------------------------\n\n",
+    "Hosting for the PostgreSQL Buildfarm is generously ",
+    "provided by: CommandPrompt, The PostgreSQL Company";
+
+    exit;
+
+}
+
+else 
+{
+    print "Status: 460 bad parameters\n",
+    "Content-Type: text/plain\n\n";
+    exit;
+}
+
 if ($system && $logdate)
 {
 
