732fa93623a0619eff6de771d537dc1c15706831
Andrew Dunstan <andrew@dunslane.net>
2010-10-11 20:51:07 +0000

    remove redundant lines

--- cgi-bin/show_stage_log.pl
+++ cgi-bin/show_stage_log.pl
@@ -61,82 +61,11 @@ if ($system && $logdate && $stage)
     "Hosting for the PostgreSQL Buildfarm is generously ",
     "provided by: CommandPrompt, The PostgreSQL Company";
 
-    exit;
-
 }
 
 else 
 {
     print "Status: 460 bad parameters\n",
     "Content-Type: text/plain\n\n";
-    exit;
 }
 
-if ($system && $logdate)
-{
-
-    my $db = DBI->connect($dsn,$dbuser,$dbpass);
-
-    die $DBI::errstr unless $db;
-
-    my $statement = q(
-
-		select log_archive
-		from build_status
-		where sysname = ? and snapshot = ?
-
-		);
-
-
-    
-    my $sth=$db->prepare($statement);
-    $sth->execute($system,$logdate);
-    my $row=$sth->fetchrow_arrayref;
-    $tgz=$row->[0];
-    $sth->finish;
-    $db->disconnect;
-
-}
-
-unless ($stage)
-{
-
-    print 
-	"Content-Type: application/x-gzip\n", 
-	"Content-Disposition: attachment; filename=buildfarmlog.tgz\n",
-	"\n",
-	$tgz;
-    exit;
-}
-
-my $template = "buildlogXXXXXX";
-my ($fh, $filename) = tempfile($template, 
-							   DIR => '/home/community/pgbuildfarm/buildlogs',
-							   UNLINK => 1);
-print $fh $tgz;
-close($fh);
-
-my $output = `tar -z -O -xf $filename $stage.log 2>&1`;
-
-print "Content-Type: text/plain\n\n", $output,
-
-    "-------------------------------------------------\n\n",
-    "Hosting for the PostgreSQL Buildfarm is generously ",
-    "provided by: CommandPrompt, The PostgreSQL Company";
-
-; exit;
-
-# using <pre> like this on huge files can make browsers choke
-
-print "Content-Type: text/html\n\n";
-
-print <<EOHTML;
-<html>
-<body>
-<pre>
-$output
-</pre>
-</body>
-</html>
-
-EOHTML
