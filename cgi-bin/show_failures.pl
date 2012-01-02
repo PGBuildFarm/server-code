2af257e0d895751efaf3d9db9d69b5c5c04c3606
Andrew Dunstan <andrew@dunslane.net>
2012-01-02 06:55:01 -0800

    add filtering to failures page.

--- cgi-bin/show_failures.pl
+++ cgi-bin/show_failures.pl
@@ -19,9 +19,13 @@ use vars qw($dbhost $dbname $dbuser $dbpass $dbport $template_dir);
 require "$ENV{BFConfDir}/BuildFarmWeb.pl";
 
 my $query = new CGI;
-my @members = $query->param('member');
+my @members = grep {$_ ne "" } $query->param('member');
 map { s/[^a-zA-Z0-9_ -]//g; } @members;
 my $max_days =  $query->param('max_days') + 0 || 10;
+my @branches = grep {$_ ne "" } $query->param('branch');
+map { s/[^a-zA-Z0-9_ -]//g; } @branches;
+my @stages = grep {$_ ne "" } $query->param('stage');
+map { s/[^a-zA-Z0-9_ -]//g; } @stages;
 
 my $dsn="dbi:Pg:dbname=$dbname";
 $dsn .= ";host=$dbhost" if $dbhost;
@@ -42,6 +46,38 @@ elsif ($sortby eq 'namenobranch')
 
 my $db = DBI->connect($dsn,$dbuser,$dbpass,{pg_expand_array => 0}) 
     or die("$dsn,$dbuser,$dbpass,$!");
+
+my $get_all_branches = qq{
+
+  select distinct branch
+  from nrecent_failures
+  where branch <> 'HEAD'
+  order by branch desc
+
+};
+
+my $all_branches = $db->selectcol_arrayref($get_all_branches);
+unshift (@$all_branches,'HEAD');
+
+my $get_all_members = qq{
+
+  select distinct sysname
+  from nrecent_failures
+  order by sysname
+
+};
+
+my $all_members = $db->selectcol_arrayref($get_all_members);
+
+my $get_all_stages = qq{
+
+  select distinct stage 
+  from build_status 
+    join nrecent_failures using (sysname,snapshot,branch)
+
+};
+
+my $all_stages = $db->selectcol_arrayref($get_all_stages);
 
 my $statement =<<EOS;
 
@@ -69,6 +105,8 @@ $sth->execute($max_days);
 while (my $row = $sth->fetchrow_hashref)
 {
     next if (@members && ! grep {$_ eq $row->{sysname} } @members);
+    next if (@stages && ! grep {$_ eq $row->{stage} } @stages);
+    next if (@branches && ! grep {$_ eq $row->{branch} } @branches);
     $row->{build_flags}  =~ s/^\{(.*)\}$/$1/;
     $row->{build_flags}  =~ s/,/ /g;
 	# enable-integer-datetimes is now the default
@@ -103,7 +141,12 @@ $template->process('fstatus.tt',
 		{statrows=>$statrows, 
 		 sortby => $sortby,
 		 max_days => $max_days,
-		 members=> \@members} );
+		 all_branches => $all_branches,
+		 all_members => $all_members,
+		 all_stages => $all_stages,
+		 qmembers=> \@members,
+		 qbranches => \@branches,
+		 qstages => \@stages} );
 
 exit;
 
