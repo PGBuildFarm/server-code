0825fb4dbfcb9066c625db2fc2961c694b3eefee
Andrew Dunstan <andrew@dunslane.net>
2016-02-02 18:00:07 -0500

    perl replacement for python

---
+++ bin/applications_reminder.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+
+=comment
+
+Copyright (c) 2003-2010, Andrew Dunstan
+
+See accompanying License file for license details
+
+=cut 
+
+use strict;
+
+use DBI;
+use DBD::Pg;
+use Mail::Send;
+
+use vars qw($dbhost $dbname $dbuser $dbpass $dbport 
+            $default_host $reminders_from $notifyapp);
+
+require "$ENV{BFConfDir}/BuildFarmWeb.pl";
+
+die "no dbname" unless $dbname;
+die "no dbuser" unless $dbuser;
+
+# don't use configged dbuser/dbpass
+
+$dbuser=""; $dbpass="";
+
+my $dsn="dbi:Pg:dbname=$dbname";
+$dsn .= ";host=$dbhost" if $dbhost;
+$dsn .= ";port=$dbport" if $dbport;
+
+my $db = DBI->connect($dsn,$dbuser,$dbpass);
+
+die $DBI::errstr unless $db;
+
+my $rows = $db->selectall_arrayref("SELECT * FROM pending()",{ Slice => {} });
+
+$db->disconnect;
+
+exit unless $rows && @$rows;
+
+my $me = `id -un`; chomp $me;
+my $host = `hostname`; chomp ($host);
+$host = $default_host unless ($host =~ m/[.]/ || !defined($default_host));
+
+my $from_addr = "PG Build Farm <$me\@$host>";
+$from_addr =~ tr /\r\n//d;
+
+$from_addr = $reminders_from if $reminders_from;
+
+my $msg = new Mail::Send;
+$msg->set('From',$from_addr);
+$msg->to($notifyapp);
+$msg->subject("PGBuildfarm pending applications reminder");
+my $fh = $msg->open;
+
+print $fh "\nOutstanding buildfarm application(s) still pending: \n\n";
+foreach my $row (@$rows)
+{
+	printf $fh
+	  "%s(%s) - %s(%s) on %s\n - compiler: %s v%s\n - pending since: %s\n\n",
+        $row->{'owner'}, 
+        $row->{'owner_email'}, 
+        $row->{'operating_system'},
+        $row->{'os_version'},
+        $row->{'architecture'},
+        $row->{'compiler'},
+        $row->{'compiler_version'},
+        $row->{'status_ts'};
+}
+$fh->close;
+
+
