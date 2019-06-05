ba0e537f416c5b17be0678e02347b4e56a98cccb
Andrew Dunstan <andrew@dunslane.net>
2019-06-05 08:11:24 -0400

    suppress envtest.pl output unless specifically enabled

--- cgi-bin/envtest.pl
+++ cgi-bin/envtest.pl
@@ -5,7 +5,13 @@ use warnings;
 
 print "Content-Type: text/plain\n\n";
 
+use vars qw($envtestenabled);
+
 $ENV{BFConfDir} ||= $ENV{BFCONFDIR} if exists $ENV{BFCONFDIR};
+require "$ENV{BFConfDir}/BuildFarmWeb.pl";
+
+exit unless $envtestenabled;
+
 print "Conf: $ENV{BFConfDir}\n";
 
 print `pwd`;
