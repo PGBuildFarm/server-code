620b2b8314edc7f813f013dbc7833cf3c35e957a
Andrew Dunstan <andrew@dunslane.net>
2020-06-12 08:53:53 -0400

    rename tgz file with animal and timestamp to match txn file

--- cgi-bin/pgstatus.pl
+++ cgi-bin/pgstatus.pl
@@ -617,7 +617,9 @@ if ($ENV{BF_DEBUG})
 	# but turn the space into a T like iso 8601
 	my $ndbdate = $dbdate;
 	$ndbdate =~ s/ /T/g;
-	move "$buildlogs/$animal.$date", "$buildlogs/$animal.$ndbdate"
+	move "$buildlogs/$animal.$date", "$buildlogs/$animal.$ndbdate";
+	move "$buildlogs/tmp.$$.tgz", "$buildlogs/$animal.$ndbdate.tgz"
+	  if $log_archive;
 }
 
 my $url = $status_url;
