2bb8d19b956084516c2c1e5d4ff0ee09a19c3b73
PG Buildfarm <buildfarm-admins@lists.postgresql.org>
2020-06-20 19:44:06 +0000

    update sample script

--- bin/pull_sample_data.sh
+++ bin/pull_sample_data.sh
@@ -10,9 +10,9 @@ cd $DIR
 
 psql -q pgbfprod <<'EOF'
 
-set client_encoding = 'sql_ascii';
-\copy (select * from build_status_log x where sysname in ('prion','drongo') and branch = 'HEAD' and exists (select 1 from dashboard_mat d where d.sysname = x.sysname and d.snapshot = x.snapshot)) to build_status_log.data
-\copy (select * from build_status x where exists (select 1 from dashboard_mat d where d.sysname = x.sysname and d.snapshot = x.snapshot)) to build_status.data
+set client_encoding = 'UTF8';
+\copy (select * from build_status_log_raw x where sysname in ('prion','drongo') and branch = 'HEAD' and exists (select 1 from dashboard_mat d where d.sysname = x.sysname and d.snapshot = x.snapshot)) to build_status_log_raw.data
+\copy (select * from build_status_raw x where exists (select 1 from dashboard_mat d where d.sysname = x.sysname and d.snapshot = x.snapshot)) to build_status_raw.data
 \copy (select * from build_status_recent_500 where report_time > now() - interval '90 days') to build_status_recent_500.data
 \copy (select name,name,operating_system,os_version,compiler,compiler_version,architecture,status,'foo'::text,'foo@bar.baz'::text,status_ts,no_alerts,sys_notes,sys_notes_ts from public.buildsystems) to buildsystems.data
 \copy alerts to alerts.data
@@ -27,19 +27,19 @@ EOF
 cat > load-sample-data.sql <<EOF
 
 begin;
-set client_encoding = 'sql_ascii';
-alter table build_status disable trigger user;
+set client_encoding = 'UTF8';
+alter table build_status_raw disable trigger user;
 \copy buildsystems from buildsystems.data
 \copy alerts from alerts.data
-\copy build_status from build_status.data
-\copy build_status_log from build_status_log.data
+\copy build_status_raw from build_status_raw.data
+\copy build_status_log_raw from build_status_log_raw.data
 \copy build_status_recent_500 from build_status_recent_500.data
 \copy dashboard_last_modified from dashboard_last_modified.data
 \copy dashboard_mat from dashboard_mat.data
 \copy latest_snapshot from latest_snapshot.data
 \copy nrecent_failures from nrecent_failures.data
 \copy personality from personality.data
-alter table build_status enable trigger user;
+alter table build_status_raw enable trigger user;
 commit;
 
 EOF
@@ -47,19 +47,19 @@ EOF
 cat > unload-sample-data.sql <<EOF
 
 begin;
-set client_encoding = 'sql_ascii';
-alter table build_status disable trigger user;
+set client_encoding = 'UTF8';
+alter table build_status_raw disable trigger user;
 truncate buildsystems
  ,  alerts
- ,  build_status
- ,  build_status_log
+ ,  build_status_raw
+ ,  build_status_log_raw
  ,  build_status_recent_500
  ,  dashboard_last_modified
  ,  dashboard_mat
  ,  latest_snapshot
  ,  nrecent_failures
  ,  personality;
-alter table build_status enable trigger user;
+alter table build_status_raw enable trigger user;
 commit;
 
 EOF
